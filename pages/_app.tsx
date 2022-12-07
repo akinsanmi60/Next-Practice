d0e794115aed71b7b27b2fc905138158552995e8
Akinsanmi <akintunde60@gmail.com>
2022-12-07 13:23:43 +0100

    added document file in page folder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,23 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import LayoutWrapper from "../utils/WebpageWrap";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient());
+
   return (
     <ChakraProvider>
       <LayoutWrapper>
-        <Component {...pageProps} />
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={pageProps.dehydratedState}>
+            <Component {...pageProps} />
+          </Hydrate>
+        </QueryClientProvider>
       </LayoutWrapper>
     </ChakraProvider>
   );
---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+
+class CustomDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en-US">
+        <Head>
+          <link
+            rel="stylesheet"
+            crossOrigin="anonymous"
+            href="https://fonts.googleapis.com/css2?family=Crimson+Text:wght@600;700&family=DM+Sans:wght@400;500;700&display=swap"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
